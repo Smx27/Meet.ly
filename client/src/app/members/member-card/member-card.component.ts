7a70e5e118002be4f6acd9e4a2d514f0d878e0b7
sumit <maiti.sumit2001@gmail.com>
2023-08-27 18:29:06 +0530

    Added JWT interceptor

---
+++ client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+
+@Component({
+  selector: 'app-member-card',
+  templateUrl: './member-card.component.html',
+  styleUrls: ['./member-card.component.css']
+})
+export class MemberCardComponent implements OnInit {
+  @Input() member: Member | undefined; // member is an input property, so it can be passed in from the member-list component
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

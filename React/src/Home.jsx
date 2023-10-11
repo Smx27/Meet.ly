32de234ecd4113f8d8777b3aa9eda37429b1808c
codewithpriya-522 <106175873+codewithpriya-522@users.noreply.github.com>
2023-10-12 00:59:16 +0530

    add footer and hero section3

--- React/src/Home.jsx
+++ React/src/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from './component/navbar/NavBar'
 import Hero from './component/hero/Hero'
+import Footer from './component/footer/Footer'
 
 const Home = () => {
   return (
     <div >
         <NavBar/>
         <Hero/>
+        <Footer/>
     </div>
   )
 }

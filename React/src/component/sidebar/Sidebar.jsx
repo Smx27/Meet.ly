7336ef84eded04af3bd9dbb596e07c1d64bbf8ae
codewithpriya-522 <106175873+codewithpriya-522@users.noreply.github.com>
2024-02-05 01:02:37 +0530

    edit sidebar

--- React/src/component/sidebar/Sidebar.jsx
+++ React/src/component/sidebar/Sidebar.jsx
@@ -5,9 +5,6 @@ const Sidebar = () => {
   return (
 <div className="min-h-screen flex flex-col flex-auto flex-shrink-0 antialiased bg-gray-50 text-gray-800">
   <div className="fixed flex flex-col top-0 left-0 w-64 bg-white h-full border-r">
-    <div className="flex items-center justify-center h-14 border-b">
-      <div>Sidebar Navigation By iAmine</div>
-    </div>
     <div className="overflow-y-auto overflow-x-hidden flex-grow">
       <ul className="flex flex-col py-4 space-y-1">
         <li className="px-5">
@@ -15,14 +12,7 @@ const Sidebar = () => {
             <div className="text-sm font-light tracking-wide text-gray-500">Menu</div>
           </div>
         </li>
-        <li>
-          <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-            <span className="inline-flex justify-center items-center ml-4">
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-            </span>
-            <span className="ml-2 text-sm tracking-wide truncate">Dashboard</span>
-          </a>
-        </li>
+       
         <li>
           <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
             <span className="inline-flex justify-center items-center ml-4">
@@ -49,28 +39,9 @@ const Sidebar = () => {
             <span className="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-red-500 bg-red-50 rounded-full">1.2k</span>
           </a>
         </li>
-        <li className="px-5">
-          <div className="flex flex-row items-center h-8">
-            <div className="text-sm font-light tracking-wide text-gray-500">Tasks</div>
-          </div>
-        </li>
-        <li>
-          <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-            <span className="inline-flex justify-center items-center ml-4">
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01" /></svg>
-            </span>
-            <span className="ml-2 text-sm tracking-wide truncate">Available Tasks</span>
-          </a>
-        </li>
-        <li>
-          <a href="#" className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6">
-            <span className="inline-flex justify-center items-center ml-4">
-              <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" /></svg>
-            </span>
-            <span className="ml-2 text-sm tracking-wide truncate">Clients</span>
-            <span className="px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-green-500 bg-green-50 rounded-full">15</span>
-          </a>
-        </li>
+       
+       
+        
         <li className="px-5">
           <div className="flex flex-row items-center h-8">
             <div className="text-sm font-light tracking-wide text-gray-500">Settings</div>
--- React/src/routing/HomeRouting.jsx
+++ React/src/routing/HomeRouting.jsx
@@ -12,7 +12,7 @@ const HomeRouting = () => {
           </div>
         </div>
       </div> */}
-      priya
+     
     </React.Fragment>
   );
 };
